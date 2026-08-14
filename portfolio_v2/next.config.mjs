9ead01af0c7d04a3a953e988ea918b1c985a666a
Marosata <marosataf@gmail.com>
2026-08-14 22:46:38 +0300

    fix the default redirection

--- portfolio_v2/next.config.mjs
+++ portfolio_v2/next.config.mjs
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true
-  },
   i18n: {
     locales: ["en", "fr"],
-    defaultLocale: "en"
+    defaultLocale: "en",
+    localeDetection: false
   }
 };
 
