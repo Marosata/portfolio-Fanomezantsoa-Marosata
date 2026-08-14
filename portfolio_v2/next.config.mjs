fc96f5d0454450fe321c1e4254a663a5466941af
Marosata <marosataf@gmail.com>
2026-08-14 22:29:26 +0300

    [FEAT]: Mis à jour de node js

--- portfolio_v2/next.config.mjs
+++ portfolio_v2/next.config.mjs
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true
-  },
+
   i18n: {
     locales: ["en", "fr"],
-    defaultLocale: "en"
-  }
+    defaultLocale: "en",
+  },
 };
 
 export default nextConfig;
-
-
